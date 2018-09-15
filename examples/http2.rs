7748becf568c9ab7867061852753b24d9f252b56
Stephen M. Coakley <me@stephencoakley.com>
2018-09-14 23:59:23 -0500

    Enable HTTP2 feature

--- examples/http2.rs
+++ examples/http2.rs
@@ -1,16 +1,13 @@
 extern crate chttp;
 
-
 fn main() {
-    let mut options = chttp::Options::default();
-    options.preferred_http_version = Some(chttp::http::Version::HTTP_2);
-
-    let client = chttp::Client::builder()
-        .options(options)
-        .build()
+    let response = chttp::http::Request::get("https://nghttp2.org")
+        .extension(chttp::Options::default()
+            .with_preferred_http_version(Some(chttp::http::Version::HTTP_2)))
+        .body(())
+        .map_err(Into::into)
+        .and_then(chttp::send)
         .unwrap();
 
-    let mut response = client.get("https://nghttp2.org").unwrap();
-    let body = response.body_mut().text().unwrap();
-    println!("{}", body);
+    println!("{:?}", response.headers());
 }
