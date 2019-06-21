651fb9f549ee0a2ec2eae7476fbbf126a9beb536
Stephen M. Coakley <me@stephencoakley.com>
2019-06-21 09:57:59 -0500

    Fix compiler warning

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 use std::fs;
 use std::path::PathBuf;
 
-fn main() -> Result<(), Box<Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR")?);
     fs::write(out_dir.join("features.txt"), get_feature_string())?;
 
