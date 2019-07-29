267dba03f3e3d10334288f8ebedf28c0658d2f7f
Stephen M. Coakley <me@stephencoakley.com>
2019-07-29 11:29:56 -0500

    Update async.rs

--- examples/async.rs
+++ examples/async.rs
@@ -1,5 +1,5 @@
-//! This example demonstrates the use of `send_async()` (incubating) to make a
-//! request asynchronously using the unstable futures API.
+//! This example demonstrates the use of `send_async()` to make a request
+//! asynchronously using the futures-based API.
 #![cfg(feature = "nightly")]
 #![feature(async_await)]
 
@@ -7,13 +7,14 @@ use chttp::prelude::*;
 
 fn main() -> Result<(), chttp::Error> {
     futures::executor::block_on(async {
-        let response = Request::get("http://example.org")
+        let mut response = Request::get("http://example.org")
             .body(())?
             .send_async()
             .await?;
 
         println!("Status: {}", response.status());
         println!("Headers:\n{:?}", response.headers());
+        println!("Body: {}", response.text_async().await?);
 
         Ok(())
     })
