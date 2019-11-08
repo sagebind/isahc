dea09099462c0e4c1f03eee4936b22019881f007
Stephen M. Coakley <me@stephencoakley.com>
2019-11-07 20:33:01 -0600

    Start with a more conservative API

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,11 +1,7 @@
 //! Request and response metrics tracking.
 
 use crossbeam_utils::atomic::AtomicCell;
-use std::{
-    fmt,
-    sync::Arc,
-    time::Duration,
-};
+use std::{fmt, sync::Arc, time::Duration};
 
 /// An object that holds status updates and progress statistics on a particular
 /// request. A [`Metrics`] can be shared between threads, which allows an agent
@@ -24,14 +20,30 @@ pub(crate) struct Inner {
     pub(crate) upload_total: AtomicCell<f64>,
     pub(crate) download_progress: AtomicCell<f64>,
     pub(crate) download_total: AtomicCell<f64>,
+
     pub(crate) upload_speed: AtomicCell<f64>,
     pub(crate) download_speed: AtomicCell<f64>,
-    pub(crate) total_time: AtomicCell<f64>,
+
+    // An overview of the six time values (taken from the curl documentation):
+    //
+    // curl_easy_perform()
+    //     |
+    //     |--NAMELOOKUP
+    //     |--|--CONNECT
+    //     |--|--|--APPCONNECT
+    //     |--|--|--|--PRETRANSFER
+    //     |--|--|--|--|--STARTTRANSFER
+    //     |--|--|--|--|--|--TOTAL
+    //     |--|--|--|--|--|--REDIRECT
+    //
+    // The numbers we expose in the API are a little more "high-level" than the
+    // ones written here.
     pub(crate) namelookup_time: AtomicCell<f64>,
     pub(crate) connect_time: AtomicCell<f64>,
     pub(crate) appconnect_time: AtomicCell<f64>,
     pub(crate) pretransfer_time: AtomicCell<f64>,
     pub(crate) starttransfer_time: AtomicCell<f64>,
+    pub(crate) total_time: AtomicCell<f64>,
     pub(crate) redirect_time: AtomicCell<f64>,
 }
 
@@ -73,47 +85,53 @@ impl Metrics {
     ///
     /// When a redirect is followed, the time from each request is added
     /// together.
-    pub fn namelookup_time(&self) -> Duration {
+    pub fn name_lookup_time(&self) -> Duration {
         Duration::from_secs_f64(self.inner.namelookup_time.load())
     }
 
-    /// Get the total time from the start of the request until the connection to
-    /// the remote host (or proxy) was completed.
+    /// Get the amount of time taken to establish a connection to the server
+    /// (not including TLS connection time).
     ///
     /// When a redirect is followed, the time from each request is added
     /// together.
     pub fn connect_time(&self) -> Duration {
-        Duration::from_secs_f64(self.inner.connect_time.load())
+        Duration::from_secs_f64(
+            (self.inner.connect_time.load() - self.inner.namelookup_time.load()).max(0f64),
+        )
     }
 
-    /// Get the time it took from the start of the request until the SSL/SSH
-    /// connect/handshake to the remote host was completed.
+    /// Get the amount of time spent on TLS handshakes.
     ///
     /// When a redirect is followed, the time from each request is added
     /// together.
-    pub fn appconnect_time(&self) -> Duration {
-        Duration::from_secs_f64(self.inner.appconnect_time.load())
-    }
+    pub fn secure_connect_time(&self) -> Duration {
+        let app_connect_time = self.inner.appconnect_time.load();
 
-    /// Get the time it took from the start of the request until the file
-    /// transfer is just about to begin. This includes all pre-transfer commands
-    /// and negotiations that are specific to the particular protocol(s)
-    /// involved. It does not involve the sending of the protocol-specific
-    /// request that triggers a transfer.
-    ///
-    /// When a redirect is followed, the time from each request is added
-    /// together.
-    pub fn pretransfer_time(&self) -> Duration {
-        Duration::from_secs_f64(self.inner.pretransfer_time.load())
+        if app_connect_time > 0f64 {
+            Duration::from_secs_f64(app_connect_time - self.inner.connect_time.load())
+        } else {
+            Duration::new(0, 0)
+        }
     }
 
     /// Get the time it took from the start of the request until the first
-    /// response byte is received.
+    /// byte is either sent or received.
     ///
     /// When a redirect is followed, the time from each request is added
     /// together.
-    pub fn starttransfer_time(&self) -> Duration {
+    pub fn transfer_start_time(&self) -> Duration {
         Duration::from_secs_f64(self.inner.starttransfer_time.load())
+    }
+
+    /// Get the amount of time spent performing the actual request transfer. The
+    /// "transfer" includes both sending the request and receiving the response.
+    ///
+    /// When a redirect is followed, the time from each request is added
+    /// together.
+    pub fn transfer_time(&self) -> Duration {
+        Duration::from_secs_f64(
+            (self.inner.total_time.load() - self.inner.starttransfer_time.load()).max(0f64),
+        )
     }
 
     /// Get the total time for the entire request. This will continuously
@@ -140,20 +158,13 @@ impl fmt::Debug for Metrics {
             .field("upload_speed", &self.upload_speed())
             .field("download_progress", &self.download_progress())
             .field("download_speed", &self.download_speed())
-            .field("namelookup_time", &self.namelookup_time())
+            .field("name_lookup_time", &self.name_lookup_time())
             .field("connect_time", &self.connect_time())
-            .field("appconnect_time", &self.appconnect_time())
-            .field("pretransfer_time", &self.pretransfer_time())
-            .field("starttransfer_time", &self.starttransfer_time())
+            .field("secure_connect_time", &self.secure_connect_time())
+            .field("transfer_start_time", &self.transfer_start_time())
+            .field("transfer_time", &self.transfer_time())
             .field("total_time", &self.total_time())
             .field("redirect_time", &self.redirect_time())
             .finish()
-    }
-}
-
-impl fmt::Display for Metrics {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "Upload")?;
-        writeln!(f, "{}/{}", self.upload_progress().0, self.upload_progress().1)
     }
 }
