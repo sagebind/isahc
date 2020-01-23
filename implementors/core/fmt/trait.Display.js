29bf6e7cfa7ebb4c22eca898a580b496617ac036
sagebind <me@stephencoakley.com>
2020-01-23 03:07:40 +0000

    Deploying to gh-pages from  - 6eb20182d3047fd988a9acb0082f863391a088b9 🚀

--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["isahc"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"isahc/enum.Error.html\" title=\"enum isahc::Error\">Error</a>","synthetic":false,"types":["isahc::error::Error"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/future/future/trait.Future.js
+++ implementors/core/future/future/trait.Future.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["isahc"] = [{"text":"impl&lt;'_&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"isahc/struct.ResponseFuture.html\" title=\"struct isahc::ResponseFuture\">ResponseFuture</a>&lt;'_&gt;","synthetic":false,"types":["isahc::client::ResponseFuture"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
