c9b6ef2625ba03ae08656fe7e7f8001f5459e2aa
sagebind <me@stephencoakley.com>
2020-07-29 01:42:54 +0000

    Deploying to gh-pages from  @ c5f83e1b1938c74b33b138d488f53d4f9477def7 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["isahc"] = {"name":"","dirs":[{"name":"config","files":["dns.rs","internal.rs","mod.rs","proxy.rs","redirect.rs","ssl.rs"]}],"files":["agent.rs","auth.rs","body.rs","client.rs","error.rs","handler.rs","lib.rs","metrics.rs","middleware.rs","parse.rs","request.rs","response.rs","task.rs","text.rs"]};
+sourcesIndex["isahc"] = {"name":"","dirs":[{"name":"config","files":["dial.rs","dns.rs","internal.rs","mod.rs","proxy.rs","redirect.rs","ssl.rs"]}],"files":["agent.rs","auth.rs","body.rs","client.rs","error.rs","handler.rs","lib.rs","metrics.rs","middleware.rs","parse.rs","request.rs","response.rs","task.rs","text.rs"]};
 createSourceSidebar();
