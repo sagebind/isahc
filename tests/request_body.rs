f326324173b67eba40fe9530e3261259e6bd0402
Stephen M. Coakley <me@stephencoakley.com>
2020-09-23 00:09:42 -0500

    Unroll iterating tests with test-case

--- tests/request_body.rs
+++ tests/request_body.rs
@@ -1,73 +1,86 @@
 use isahc::prelude::*;
 use isahc::Body;
 use mockito::{mock, server_url};
+use test_case::test_case;
 
-#[test]
-fn request_with_body_of_known_size() {
-    for method in &["GET", "HEAD", "POST", "PUT", "DELETE", "PATCH", "FOOBAR"] {
-        let body = "MyVariableOne=ValueOne&MyVariableTwo=ValueTwo";
+#[test_case("GET")]
+#[test_case("HEAD")]
+#[test_case("POST")]
+#[test_case("PUT")]
+#[test_case("DELETE")]
+#[test_case("PATCH")]
+#[test_case("FOOBAR")]
+fn request_with_body_of_known_size(method: &str) {
+    let body = "MyVariableOne=ValueOne&MyVariableTwo=ValueTwo";
 
-        let m = mock(method, "/")
-            .match_header("content-type", "application/x-www-form-urlencoded")
-            .match_body(body)
-            .create();
+    let m = mock(method, "/")
+        .match_header("content-type", "application/x-www-form-urlencoded")
+        .match_body(body)
+        .create();
 
-        Request::builder()
-            .method(*method)
-            .uri(server_url())
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(body)
-            .unwrap()
-            .send()
-            .unwrap();
+    Request::builder()
+        .method(method)
+        .uri(server_url())
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .unwrap()
+        .send()
+        .unwrap();
 
-        m.assert();
-    }
+    m.assert();
 }
 
-#[test]
-fn request_with_body_of_unknown_size_uses_chunked_encoding() {
-    for method in &["GET", "HEAD", "POST", "PUT", "DELETE", "PATCH", "FOOBAR"] {
-        let body = "foo";
+#[test_case("GET")]
+#[test_case("HEAD")]
+#[test_case("POST")]
+#[test_case("PUT")]
+#[test_case("DELETE")]
+#[test_case("PATCH")]
+#[test_case("FOOBAR")]
+fn request_with_body_of_unknown_size_uses_chunked_encoding(method: &str) {
+    let body = "foo";
 
-        let m = mock(method, "/")
-            .match_header("transfer-encoding", "chunked")
-            .match_body(body)
-            .create();
+    let m = mock(method, "/")
+        .match_header("transfer-encoding", "chunked")
+        .match_body(body)
+        .create();
 
-        Request::builder()
-            .method(*method)
-            .uri(server_url())
-            // This header should be ignored
-            .header("transfer-encoding", "identity")
-            .body(Body::from_reader(body.as_bytes()))
-            .unwrap()
-            .send()
-            .unwrap();
+    Request::builder()
+        .method(method)
+        .uri(server_url())
+        // This header should be ignored
+        .header("transfer-encoding", "identity")
+        .body(Body::from_reader(body.as_bytes()))
+        .unwrap()
+        .send()
+        .unwrap();
 
-        m.assert();
-    }
+    m.assert();
 }
 
-#[test]
 #[ignore]
-fn content_length_header_takes_precedence_over_body_objects_length() {
-    for method in &["GET", "HEAD", "POST", "PUT", "DELETE", "PATCH", "FOOBAR"] {
-        let m = mock(method, "/")
-            .match_header("content-length", "3")
-            .match_body("abc") // Truncated to 3 bytes
-            .create();
+#[test_case("GET")]
+#[test_case("HEAD")]
+#[test_case("POST")]
+#[test_case("PUT")]
+#[test_case("DELETE")]
+#[test_case("PATCH")]
+#[test_case("FOOBAR")]
+fn content_length_header_takes_precedence_over_body_objects_length(method: &str) {
+    let m = mock(method, "/")
+        .match_header("content-length", "3")
+        .match_body("abc") // Truncated to 3 bytes
+        .create();
 
-        Request::builder()
-            .method(*method)
-            .uri(server_url())
-            // Override given body's length
-            .header("content-length", "3")
-            .body("abc123")
-            .unwrap()
-            .send()
-            .unwrap();
+    Request::builder()
+        .method(method)
+        .uri(server_url())
+        // Override given body's length
+        .header("content-length", "3")
+        .body("abc123")
+        .unwrap()
+        .send()
+        .unwrap();
 
-        m.assert();
-    }
+    m.assert();
 }
--- tests/status.rs
+++ tests/status.rs
@@ -1,15 +1,26 @@
 use mockito::{mock, server_url};
+use test_case::test_case;
 
-#[test]
-fn returns_correct_response_code() {
-    for status in [200u16, 202, 204, 302, 308, 400, 403, 404, 418, 429, 451, 500, 503].iter() {
-        let m = mock("GET", "/")
-            .with_status(*status as usize)
-            .create();
+#[test_case(200)]
+#[test_case(202)]
+#[test_case(204)]
+#[test_case(302)]
+#[test_case(308)]
+#[test_case(400)]
+#[test_case(403)]
+#[test_case(404)]
+#[test_case(418)]
+#[test_case(429)]
+#[test_case(451)]
+#[test_case(500)]
+#[test_case(503)]
+fn returns_correct_response_code(status: u16) {
+    let m = mock("GET", "/")
+        .with_status(status as usize)
+        .create();
 
-        let response = isahc::get(server_url()).unwrap();
+    let response = isahc::get(server_url()).unwrap();
 
-        assert_eq!(response.status(), *status);
-        m.assert();
-    }
+    assert_eq!(response.status(), status);
+    m.assert();
 }
