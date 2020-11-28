341c7c539af84bef77f57fd7b2cabad88093b8ac
sagebind <me@stephencoakley.com>
2020-11-28 19:38:53 +0000

    Deploying to gh-pages from  @ 5b17d4c13b1c2140a75920eca3b3609011d7084c 🚀

--- implementors/isahc/trait.ReadResponseExt.js
+++ implementors/isahc/trait.ReadResponseExt.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["isahc"] = [{"text":"impl AsyncRead for AsyncBody","synthetic":false,"types":[]}];
+implementors["isahc"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
