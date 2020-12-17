d8589a261b567503d15d38b32161d6b6b0777cdf
sagebind <me@stephencoakley.com>
2020-12-17 04:16:23 +0000

    Deploying to gh-pages from  @ dd3745bc897b191cc7105a8511b9809f8c4fc0b7 🚀

--- implementors/core/marker/trait.StructuralPartialEq.js
+++ implementors/core/marker/trait.StructuralPartialEq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["isahc"] = [{"text":"impl StructuralPartialEq for ErrorKind","synthetic":false,"types":[]},{"text":"impl StructuralPartialEq for DialerParseError","synthetic":false,"types":[]},{"text":"impl StructuralPartialEq for RedirectPolicy","synthetic":false,"types":[]}];
+implementors["isahc"] = [{"text":"impl StructuralPartialEq for DialerParseError","synthetic":false,"types":[]},{"text":"impl StructuralPartialEq for RedirectPolicy","synthetic":false,"types":[]},{"text":"impl StructuralPartialEq for ErrorKind","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["isahc"] = [{"text":"impl Error for Error","synthetic":false,"types":[]},{"text":"impl Error for DialerParseError","synthetic":false,"types":[]}];
+implementors["isahc"] = [{"text":"impl Error for DialerParseError","synthetic":false,"types":[]},{"text":"impl Error for Error","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
