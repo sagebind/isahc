7a5230a9616796dd7d9cac32a21b9925c81d0655
sagebind <me@stephencoakley.com>
2022-08-19 05:10:15 +0000

    Deploying to gh-pages from  @ c9cd9a11f5eef47d7d91eae38e360060ae377f83 🚀

--- implementors/futures_io/if_std/trait.AsyncRead.js
+++ implementors/futures_io/if_std/trait.AsyncRead.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["isahc"] = [{"text":"impl AsyncRead for <a class=\"struct\" href=\"isahc/struct.AsyncBody.html\" title=\"struct isahc::AsyncBody\">AsyncBody</a>","synthetic":false,"types":["isahc::body::AsyncBody"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"isahc":[["impl AsyncRead for <a class=\"struct\" href=\"isahc/struct.AsyncBody.html\" title=\"struct isahc::AsyncBody\">AsyncBody</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/std/io/trait.Read.js
+++ implementors/std/io/trait.Read.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["isahc"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"isahc/struct.Body.html\" title=\"struct isahc::Body\">Body</a>","synthetic":false,"types":["isahc::body::sync::Body"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"isahc":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"isahc/struct.Body.html\" title=\"struct isahc::Body\">Body</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
