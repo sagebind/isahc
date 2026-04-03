cc7763c085dfe09f2d14b603c94376cd61ffdeee
Stephen M. Coakley <me@stephencoakley.com>
2026-04-02 19:39:37 -0500

    Add util.rs

---
+++ src/agent/util.rs
@@ -0,0 +1,30 @@
+use std::hash::Hasher;
+
+/// Trivial hash function to use for our maps and sets that use file descriptors
+/// as keys.
+#[derive(Default)]
+pub(super) struct IntHasher([u8; 8], #[cfg(debug_assertions)] bool);
+
+impl Hasher for IntHasher {
+    fn write(&mut self, bytes: &[u8]) {
+        #[cfg(debug_assertions)]
+        {
+            if self.1 {
+                panic!("socket hash function can only be written to once");
+            } else {
+                self.1 = true;
+            }
+
+            if bytes.len() > 8 {
+                panic!("only a maximum of 8 bytes can be hashed");
+            }
+        }
+
+        (&mut self.0[..bytes.len()]).copy_from_slice(bytes);
+    }
+
+    #[inline]
+    fn finish(&self) -> u64 {
+        u64::from_ne_bytes(self.0)
+    }
+}
