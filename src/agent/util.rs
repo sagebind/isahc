fedb1a023247d05e50860d41d3a7158d3d802f4e
Stephen M. Coakley <me@stephencoakley.com>
2026-04-02 20:26:52 -0500

    Add hasher unit tests

--- src/agent/util.rs
+++ src/agent/util.rs
@@ -28,3 +28,43 @@ impl Hasher for IntHasher {
         u64::from_ne_bytes(self.0)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::IntHasher;
+    use curl::multi::Socket;
+    use quickcheck_macros::quickcheck;
+    use std::hash::{Hash, Hasher};
+
+    #[test]
+    #[should_panic]
+    fn hash_of_more_than_8_bytes_panics() {
+        "hello".hash(&mut IntHasher::default());
+    }
+
+    #[quickcheck]
+    fn hash_socket_is_deterministic(socket: Socket) {
+        let mut hasher_a = IntHasher::default();
+        socket.hash(&mut hasher_a);
+
+        let mut hasher_b = IntHasher::default();
+        socket.hash(&mut hasher_b);
+
+        assert_eq!(hasher_a.finish(), hasher_b.finish());
+    }
+
+    #[quickcheck]
+    fn hash_of_two_sockets_are_equal_iff_sockets_are_equal(a: Socket, b: Socket) {
+        let mut hasher_a = IntHasher::default();
+        a.hash(&mut hasher_a);
+
+        let mut hasher_b = IntHasher::default();
+        b.hash(&mut hasher_b);
+
+        if a == b {
+            assert_eq!(hasher_a.finish(), hasher_b.finish());
+        } else {
+            assert_ne!(hasher_a.finish(), hasher_b.finish());
+        }
+    }
+}
