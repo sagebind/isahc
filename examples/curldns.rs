026c01081f373c2aafd4856249eaae37b5ca90d6
Stephen M. Coakley <me@stephencoakley.com>
2026-05-05 22:17:29 -0500

    Use multi api

--- examples/curldns.rs
+++ examples/curldns.rs
@@ -1,11 +1,27 @@
-use curl::easy::Easy;
+use std::time::Duration;
+
+use curl::{
+    easy::{Easy, SslOpt},
+    multi::Multi,
+};
 
 fn main() -> Result<(), curl::Error> {
     let mut curl = Easy::new();
-
     curl.url("https://example.com/")?;
+    curl.ssl_options(SslOpt::new().native_ca(true))?;
     curl.verbose(true)?;
-    curl.perform()?;
+
+    let multi = Multi::new();
+    let e = multi.add(curl).unwrap();
+
+    while multi.perform().unwrap() > 0 {
+        multi.messages(|message| {
+            dbg!(message);
+        });
+        multi.wait(&mut [], Duration::from_secs(1)).unwrap();
+    }
+
+    drop(e);
 
     Ok(())
 }
